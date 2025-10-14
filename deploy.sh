04b77bf77635c8b6c92aa6b6fcfae466912bd8ce
Cevantime <thibaulttruffert@hotmail.com>
2025-10-14 12:57:29 +0200

    wrong maintenance replace order

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sed -i 's/MAINTENANCE_MODE=1/MAINTENANCE_MODE=0/' .env.local
+sed -i 's/MAINTENANCE_MODE=0/MAINTENANCE_MODE=1/' .env.local
 git reset --hard
 git pull -r
 composer install --optimize-autoloader
@@ -9,5 +9,5 @@ bin/console doctrine:migrations:migrate --no-interaction
 bin/console cache:clear
 yarn install --force
 yarn build
-sed -i 's/MAINTENANCE_MODE=0/MAINTENANCE_MODE=1/' .env.local
+sed -i 's/MAINTENANCE_MODE=1/MAINTENANCE_MODE=0/' .env.local
 chown -R www-data:www-data .
